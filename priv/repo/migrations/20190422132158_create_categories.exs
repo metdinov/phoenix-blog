50558c84c09f776fd502b8c3ea955e4b26a29e59
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2019-04-22 10:28:45 -0300

    Started blog. Added categories

---
+++ priv/repo/migrations/20190422132158_create_categories.exs
@@ -0,0 +1,11 @@
+defmodule Blog.Repo.Migrations.CreateCategories do
+  use Ecto.Migration
+
+  def change do
+    create table(:category) do
+      add :name, :string
+
+      timestamps()
+    end
+  end
+end
